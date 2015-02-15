b918d08d93b9e263242d60d16cfa4834c4ca5cb5
Alexander Geißler <lancemt@gmx.de>
2015-02-15 23:35:06 +0100

    changed documentation style and two function types

--- dac/inc/pcm1792a.h
+++ dac/inc/pcm1792a.h
@@ -36,9 +36,6 @@
 #define PCM1792A_ATTENUATION_LEFT       0x10
 #define PCM1792A_ATTENUATION_RIGHT      0x11
 #define PCM1792A_ATTENUATION_LOAD_CTRL  0x12
-#define PCM1792A_ATLD(x)                (x & 0x80)
-#define PCM1792A_FMT(x)                 (x<<4 & 0x70)
-#define PCM1792A_DFM(x)                 (x<<2 & 0x0C)
 #define PCM1792A_AUDIO_INTERFACE        0x13
 #define PCM1792A_DEEMPHASIS             0x14
 #define PCM1792A_DEVICEID               0x17
@@ -58,43 +55,49 @@
 /* Driver data structures and types.                                         */
 /*===========================================================================*/
 
-typedef struct DacProxy {
-  uint32_t sampling;    /* ! chosen samplingrate for the dac */
-  uint32_t attenuation; /* ! attenuation steps in 0.5 dB */
-  uint32_t audio_format /* ! */
-  uint32_t deemphasis;  /* ! */
-  uint32_t mute;        /* ! soft mute */
-  uint32_t dac_enable;  /* ! */
-  uint32_t rolloff;     /* ! */
-  uint32_t reset;
-  uint32_t deviceID;    /* ! */
+typedef struct Dac {
+  uint32_t sampling;                    /** chosen samplingrate for the dac */
+  uint8_t attenuation;                  /** attenuation steps in 0.5 dB */
+  AttenuationRate_t attenuation_rate    /** */
+  AudioFormat_t audio_format            /** I2S, right or left justified */
+  uint32_t deemphasis;                  /** */
+  uint32_t mute;                        /** soft mute */
+  uint32_t dac_enable;                  /** activate dac output */
+  uint32_t rolloff;                     /** set filter roll off */
+  uint32_t reset;                       /** */
+  Oversampling_t oversampling           /** choose from three oversampling rates */
+  uint32_t deviceID;                    /** chip device id */
 } DAC_t;
 
 typedef enum AudioFormat {
-  RIGHT_JUSTIFIED_16BIT,  /* ! Right justified 16 bit data */
-  RIGHT_JUSTIFIED_20BIT,  /* ! Right justified 20 bit data */
-  RIGHT_JUSTIFIED_24BIT,  /* ! Right justified 24 bit data */
-  LEFT_JUSTIFIED_24BIT,   /* ! Left justified 24 bit data MSB */
-  I2S_16BIT,              /* ! I2S with 16 bit data */
-  I2S_24BIT               /* ! I2S with 24 bit data */
+  RIGHT_JUSTIFIED_16BIT,  /** Right justified 16 bit data */
+  RIGHT_JUSTIFIED_20BIT,  /** Right justified 20 bit data */
+  RIGHT_JUSTIFIED_24BIT,  /** Right justified 24 bit data */
+  LEFT_JUSTIFIED_24BIT,   /** Left justified 24 bit data MSB */
+  I2S_16BIT,              /** I2S with 16 bit data */
+  I2S_24BIT               /** I2S with 24 bit data */
 } AudioFormat_t;
 
 typedef enum AttenuationRate {
-  LRCK1,
-  LRCK2,
-  LRCK4,
-  LRCK8
+  LRCK1, /**  default */
+  LRCK2, /**  */
+  LRCK4, /**  */
+  LRCK8  /**  */
 } AttenuationRate_t;
 
 typedef enum OversamplingRate {
-  FS_64,  /* ! Oversampling 64 times the sampling rate */
-  FS_32,  /* ! Oversampling 32 times the sampling rate */
-  FS_128  /* ! Oversampling 128 times the sampling rate */
+  FS_64,  /** Oversampling 64 times the sampling rate */
+  FS_32,  /** Oversampling 32 times the sampling rate */
+  FS_128  /** Oversampling 128 times the sampling rate */
 } Oversampling_t;
 
 /*===========================================================================*/
 /* Driver macros.                                                            */
 /*===========================================================================*/
+
+#define PCM1792A_ATLD(x)                (x & 0x80)
+#define PCM1792A_FMT(x)                 (x<<4 & 0x70)
+#define PCM1792A_DFM(x)                 (x<<2 & 0x0C)
 
 /*===========================================================================*/
 /* External declarations.                                                    */
@@ -107,8 +110,8 @@ extern "C" {
   void pcm1792aWriteRegister(SPIDriver *spip, uint8_t reg, uint8_t value);
   void DAC_initialize(DAC_t* const me);
   void DAC_configure(DAC_t* const me);
-  void DAC_attenuate(DAC_t* const me, const uint8_t attenuation);
-  void DAC_SetAttenuationRate(DAC_t* const me, const AttenuationRate_t rate);
+  void DAC_attenuate(DAC_t* const me, uint8_t attenuation);
+  void DAC_SetAttenuationRate(DAC_t* const me, AttenuationRate_t rate);
   void DAC_SetInterface(DAC_t const me, AudioFormat_t format);
   void DAC_outputEnable(DAC_t* const me);
   void DAC_deviceID(DAC_t* const me);
