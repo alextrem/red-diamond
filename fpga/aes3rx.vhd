17e1b4febd19258d2145e52cee94f33b1be16c89
Alexander Geißler <lancemt@gmx.de>
2015-02-25 23:46:57 +0100

    added architecture body and some documentation lines

--- fpga/aes3rx.vhd
+++ fpga/aes3rx.vhd
@@ -9,7 +9,14 @@ entity aes3rx is
         reset   : in std_logic;
         -- Master clock
         clk     : in std_logic;
+        -- aes/ebu, spdif compatible stream
         aes3    : in std_logic;
-        active  : out std_logic;
+        -- receiver has valid input data
+        active  : out std_logic := 0;
          );
 end aes3rx;
+
+architecture rtl of aes3rx is
+begin
+
+end rtl;
