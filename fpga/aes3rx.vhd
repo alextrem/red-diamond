0cabc37ecde6affd2d8398f59ac90ce9411c21e4
Alexander Geissler <alextrem@web.de>
2015-02-26 22:25:14 +0100

    added documentation header

--- fpga/aes3rx.vhd
+++ fpga/aes3rx.vhd
@@ -1,3 +1,20 @@
+---------------------------------------
+-- Company:
+-- Engineer:        Alexander Geißler
+--
+-- Create Date:     23:40:00 02/26/2015
+-- Design Name:
+-- Project Name:    red-diamond
+-- Target Device:   EP4CE22C8N
+-- Tool Versions:   14.0
+-- Description:
+--
+-- Dependencies:
+--
+-- Revision:
+-- Revision 0.1 - File created
+--------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
