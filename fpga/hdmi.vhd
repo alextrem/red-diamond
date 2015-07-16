145999c3114295fddfbdcf69722d1969ad0805d5
Alexander Geißler <lancemt@gmx.de>
2015-07-16 16:41:50 +0200

    -Added port definition

--- fpga/hdmi.vhd
+++ fpga/hdmi.vhd
@@ -16,3 +16,39 @@
 -- Revision 0.1 - File created
 --------------------------------------
 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity hdmi is
+    port (
+        -- Synchrounous reset
+        reset   : out std_logic;
+        -- I2C interface
+        scl     : out std_logic;
+        sda     : inout std_logic;
+        csn     : out std_logic;
+        -- I2S interface
+        -- Data outputclock for left and right channel
+        lrclk   : in std_logic;
+        -- Bit clock
+        sclk    : in std_logic;
+        -- Audio master clock output
+        mclk    : in std_logic;
+        ap0     : in std_logic;
+        ap1     : in std_logic;
+        ap2     : in std_logic;
+        ap3     : in std_logic;
+        ap4     : in std_logic;
+        -- interrupt
+        interrupt     : in std_logic_vector(1 downto 0);
+         );
+end entity
+
+architecture rtl of hdmi is
+
+    type state is (idle);
+    signal current_state : state:=idle;
+
+begin
+
+end rtl;
