5c5f1882de5469a6eda11f985deb0626eb0d4428
Alexander Geißler <lancemt@gmx.de>
2015-08-13 23:24:39 +0200

    - I2C1 Interface and I2S is activated

--- halconf.h
+++ halconf.h
@@ -76,14 +76,14 @@
  * @brief   Enables the I2C subsystem.
  */
 #if !defined(HAL_USE_I2C) || defined(__DOXYGEN__)
-#define HAL_USE_I2C                 FALSE
+#define HAL_USE_I2C                 TRUE
 #endif
 
 /**
  * @brief   Enables the I2S subsystem.
  */
 #if !defined(HAL_USE_I2S) || defined(__DOXYGEN__)
-#define HAL_USE_I2S                 FALSE
+#define HAL_USE_I2S                 TRUE
 #endif
 
 /**
