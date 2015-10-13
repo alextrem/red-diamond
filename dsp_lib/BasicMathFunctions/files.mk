40db39065dacfbf8c2791cd04b0ead53fef90937
Alexander Geißler <lancemt@gmx.de>
2015-10-13 13:26:26 +0200

    Corrected missing +

--- dsp_lib/BasicMathFunctions/files.mk
+++ dsp_lib/BasicMathFunctions/files.mk
@@ -1,37 +1,37 @@
 # CMSIS DSP Library sources
 
-DSPSRC = $(DSPLIB)/BasicMathFunctions/arm_abs_f32.c \
-         $(DSPLIB)/BasicMathFunctions/arm_abs_q15.c \
-         $(DSPLIB)/BasicMathFunctions/arm_abs_q31.c \
-         $(DSPLIB)/BasicMathFunctions/arm_abs_q7.c \
-         $(DSPLIB)/BasicMathFunctions/arm_add_f32.c \
-         $(DSPLIB)/BasicMathFunctions/arm_add_q15.c \
-         $(DSPLIB)/BasicMathFunctions/arm_add_q31.c \
-         $(DSPLIB)/BasicMathFunctions/arm_add_q7.c \
-         $(DSPLIB)/BasicMathFunctions/arm_dot_prod_f32.c \
-         $(DSPLIB)/BasicMathFunctions/arm_dot_prod_q15.c \
-         $(DSPLIB)/BasicMathFunctions/arm_dot_prod_q31.c \
-         $(DSPLIB)/BasicMathFunctions/arm_dot_prod_q7.c \
-         $(DSPLIB)/BasicMathFunctions/arm_mult_f32.c \
-         $(DSPLIB)/BasicMathFunctions/arm_mult_q15.c \
-         $(DSPLIB)/BasicMathFunctions/arm_mult_q31.c \
-         $(DSPLIB)/BasicMathFunctions/arm_mult_q7.c \
-         $(DSPLIB)/BasicMathFunctions/arm_negate_f32.c \
-         $(DSPLIB)/BasicMathFunctions/arm_negate_q15.c \
-         $(DSPLIB)/BasicMathFunctions/arm_negate_q31.c \
-         $(DSPLIB)/BasicMathFunctions/arm_negate_q7.c \
-         $(DSPLIB)/BasicMathFunctions/arm_offset_f32.c \
-         $(DSPLIB)/BasicMathFunctions/arm_offset_q15.c \
-         $(DSPLIB)/BasicMathFunctions/arm_offset_q31.c \
-         $(DSPLIB)/BasicMathFunctions/arm_offset_q7.c \
-         $(DSPLIB)/BasicMathFunctions/arm_scale_f32.c \
-         $(DSPLIB)/BasicMathFunctions/arm_scale_q15.c \
-         $(DSPLIB)/BasicMathFunctions/arm_scale_q31.c \
-         $(DSPLIB)/BasicMathFunctions/arm_scale_q7.c \
-         $(DSPLIB)/BasicMathFunctions/arm_sub_f32.c \
-         $(DSPLIB)/BasicMathFunctions/arm_sub_q15.c \
-         $(DSPLIB)/BasicMathFunctions/arm_sub_q31.c \
-         $(DSPLIB)/BasicMathFunctions/arm_sub_q7.c \
-         $(DSPLIB)/BasicMathFunctions/arm_shift_q15.c \
-         $(DSPLIB)/BasicMathFunctions/arm_shift_q31.c \
-         $(DSPLIB)/BasicMathFunctions/arm_shift_q7.c
+DSPSRC += $(DSPLIB)/BasicMathFunctions/arm_abs_f32.c \
+          $(DSPLIB)/BasicMathFunctions/arm_abs_q15.c \
+          $(DSPLIB)/BasicMathFunctions/arm_abs_q31.c \
+          $(DSPLIB)/BasicMathFunctions/arm_abs_q7.c \
+          $(DSPLIB)/BasicMathFunctions/arm_add_f32.c \
+          $(DSPLIB)/BasicMathFunctions/arm_add_q15.c \
+          $(DSPLIB)/BasicMathFunctions/arm_add_q31.c \
+          $(DSPLIB)/BasicMathFunctions/arm_add_q7.c \
+          $(DSPLIB)/BasicMathFunctions/arm_dot_prod_f32.c \
+          $(DSPLIB)/BasicMathFunctions/arm_dot_prod_q15.c \
+          $(DSPLIB)/BasicMathFunctions/arm_dot_prod_q31.c \
+          $(DSPLIB)/BasicMathFunctions/arm_dot_prod_q7.c \
+          $(DSPLIB)/BasicMathFunctions/arm_mult_f32.c \
+          $(DSPLIB)/BasicMathFunctions/arm_mult_q15.c \
+          $(DSPLIB)/BasicMathFunctions/arm_mult_q31.c \
+          $(DSPLIB)/BasicMathFunctions/arm_mult_q7.c \
+          $(DSPLIB)/BasicMathFunctions/arm_negate_f32.c \
+          $(DSPLIB)/BasicMathFunctions/arm_negate_q15.c \
+          $(DSPLIB)/BasicMathFunctions/arm_negate_q31.c \
+          $(DSPLIB)/BasicMathFunctions/arm_negate_q7.c \
+          $(DSPLIB)/BasicMathFunctions/arm_offset_f32.c \
+          $(DSPLIB)/BasicMathFunctions/arm_offset_q15.c \
+          $(DSPLIB)/BasicMathFunctions/arm_offset_q31.c \
+          $(DSPLIB)/BasicMathFunctions/arm_offset_q7.c \
+          $(DSPLIB)/BasicMathFunctions/arm_scale_f32.c \
+          $(DSPLIB)/BasicMathFunctions/arm_scale_q15.c \
+          $(DSPLIB)/BasicMathFunctions/arm_scale_q31.c \
+          $(DSPLIB)/BasicMathFunctions/arm_scale_q7.c \
+          $(DSPLIB)/BasicMathFunctions/arm_sub_f32.c \
+          $(DSPLIB)/BasicMathFunctions/arm_sub_q15.c \
+          $(DSPLIB)/BasicMathFunctions/arm_sub_q31.c \
+          $(DSPLIB)/BasicMathFunctions/arm_sub_q7.c \
+          $(DSPLIB)/BasicMathFunctions/arm_shift_q15.c \
+          $(DSPLIB)/BasicMathFunctions/arm_shift_q31.c \
+          $(DSPLIB)/BasicMathFunctions/arm_shift_q7.c
