9911258a194c6eccf7f5d8bf2ef62c844f590c37
Alexander Geißler <lancemt@gmx.de>
2015-10-13 13:25:36 +0200

    Added more Makefiles for specific dap libraries

---
+++ dsp_lib/CommonTables/files.mk
@@ -0,0 +1,4 @@
+# CMSIS DSP Library
+
+DSPSRC += $(DSPLIB)/CommonTables/arm_common_tables.c \
+          $(DSPLIB)/CommonTables/arm_const_structs.c
---
+++ dsp_lib/ControllerFunctions/files.mk
@@ -0,0 +1,11 @@
+# CMSIS DSP Library
+
+DSPSRC += $(DSPLIB)/ControllerFunctions/arm_pid_init_f32.c \
+          $(DSPLIB)/ControllerFunctions/arm_pid_init_q15.c \
+          $(DSPLIB)/ControllerFunctions/arm_pid_init_q31.c \
+          $(DSPLIB)/ControllerFunctions/arm_pid_reset_f32.c \
+          $(DSPLIB)/ControllerFunctions/arm_pid_reset_q15.c \
+          $(DSPLIB)/ControllerFunctions/arm_pid_reset_q31.c \
+          $(DSPLIB)/ControllerFunctions/arm_sin_cos_f32.c \
+          $(DSPLIB)/ControllerFunctions/arm_sin_cos_q31.c
+
