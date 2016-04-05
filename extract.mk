dde9fe75b610203927b0f64463e16ee55b205274
Alexander Geissler <gsr@izt-labs.de>
2016-04-05 09:28:24 +0200

    First working extract with 7z, some errors still show up. Needs further investigation

--- extract.mk
+++ extract.mk
@@ -1,24 +1,11 @@
-######
-# This is a Makefile to automatically extract zip sources
+# Makefile to extract 7 zip files
 #
+print-% : ;@echo $* = $($*)
 
 EXTERNAL = ChibiOS/ext
+TARFILES = $(wildcard $(EXTERNAL)/*.7z)
 
-TARFILES = $(wildcard $EXTERNAL/*.7z EXTERNAL/*.tar.gz EXTERNAL/*.tgz)
+default:
+	$(foreach file, $(TARFILES), $(shell 7z x -y -o$(EXTERNAL) $(file)))
 
-EXTRACT_TARGETS := $(addprefix .extract_, $(TARFILES))
-CLEAN_TARGETS := $(addprefix .clean_, $(TARFILES))
-
-default: $(EXTRACT_TARGETS)
-
-clean: $(CLEAN_TARGETS)
-
-$(CLEAN_TARGETS): .clean_%: .extract_%
-	rm -rf "$(shell tar tf $(patsubst .extract_%,%,$<) | head -1)"
-	rm -f $<
-
-$(EXTRACT_TARGETS): .extract_%: %
-	7z x $<
-	touch $@
-
-.PHONY: default clean $(CLEAN_TARGETS)
+.PHONY: default
