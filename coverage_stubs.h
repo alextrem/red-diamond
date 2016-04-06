0c145eca5c08e0daa31d8b0251d9eac901766c04
Alexander Geißler <lancemt@gmx.de>
2016-04-06 23:47:28 +0200

    initial commit

---
+++ coverage_stubs.h
@@ -0,0 +1,16 @@
+/*
+ * coverage_stubs.h
+ *
+ *  Created on: 26.12.2014
+ *      Author: Erich Styger
+ */
+
+#ifndef COVERAGE_STUBS_H_
+#define COVERAGE_STUBS_H_
+
+void static_init(void);
+
+void _exit(int);
+
+
+#endif /* COVERAGE_STUBS_H_ */
