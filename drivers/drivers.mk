f132b49e01ac7c3e4b7bb6191f4fe51a6529fcfe
Alexander Geißler <lancemt@gmx.de>
2016-04-21 21:07:03 +0200

    added file

--- drivers/drivers.mk
+++ drivers/drivers.mk
@@ -8,6 +8,7 @@ DRIVERDIRS = dac \
 # Source files
 
 DRIVERSRC = $(DRIVERPATH)/dac/cs43l22.c \
+            $(DRIVERPATH)/dac/pcm1792a.c \
             $(DRIVERPATH)/hdmi/adv7612.c
 
 # Include files for drivers
