d34faca0fa3493317ade024b7a321a59b6b98dce
Alexander Geißler <lancemt@gmx.de>
2016-11-19 00:12:56 +0100

    Cleaned indentation

--- fpga/spdif_pkg.vhd
+++ fpga/spdif_pkg.vhd
@@ -1,21 +1,22 @@
 ------------------------------------------------------------------------------
--- Company:
--- Engineer:        Alexander Geißler
+-- Company:          Red Diamond
+-- Engineer:         Alexander Geißler
 --
 -- Create Date:      23:40:00 02/26/2015
 -- Design Name:
 -- Project Name:     red-diamond
 -- Target Device:    EP4CE22C8N
--- Tool Versions:    14.0
--- Description:		This AES3/EBU and SPDIF receiver is compliant with
---							IEC61937, IEC60958-3 and IEC60958-4
---							This package contains constants and type definitions
---							for the receiver
+-- Tool Versions:    16.0
+-- Description:      This AES3/EBU and SPDIF receiver is compliant with
+--                   IEC61937, IEC60958-3 and IEC60958-4
+--                   This package contains constants and type definitions
+--                   for the receiver
 --
 -- Dependencies:
 --
 -- Revision:
 -- Revision 0.1 - File created
+-- Revision 0.2 - Changed indentation
 ------------------------------------------------------------------------------
 
 library IEEE;
@@ -23,38 +24,55 @@ use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 
 package spdif_pkg is
-    -- statemachine type
-    type aes3_state_type is (unlocked, confirming, locked);
+  -- statemachine type
+  type aes3_state_type is (unlocked, confirming, locked);
 
-    constant X_PREAMBLE : std_logic_vector(7 downto 0) := "01000111";
-    constant Y_PREAMBLE : std_logic_vector(7 downto 0) := "00100111";
-    constant Z_PREAMBLE : std_logic_vector(7 downto 0) := "00010111";
+  type t_aes_in is record
+    data       : std_ulogic;
+    rx_full    : std_ulogic;
+    rx_fifo_de : std_ulogic;
+  end record;
 
-    -- AES3/EBU or SPDIF stream constants
-    constant FRAMES : integer := 192;
-    subtype FRAME_WIDTH is integer range FRAMES-1 downto 0;
-    constant CHANNELS : integer := 2;
-    constant SUBFRAME : integer := 32;
+  type t_aes_out is record
+    i2s_out      : std_ulogic;
+    rx_de        : std_ulogic;
+    rx_fifo_full : std_ulogic;
+    rx_data      : std_logic_vector(31 downto 0);
+  end record;
 
-    -- channel status description
-    type byte_0 is record
-        channel_use : std_logic;
-        pcm         : std_logic;
-        preemphasis : std_logic_vector(2 downto 0);
-        --
-        sample_frequency : std_logic_vector(1 downto 0);
-    end record byte_0;
+-------------------------------------------------------------------------------
+-- Constants
+-------------------------------------------------------------------------------
 
-    type byte_1 is record
-        channel_mode    : std_logic_vector(3 downto 0);
-        user_bit        : std_logic_vector(3 downto 0);
-    end record byte_1;
+  constant X_PREAMBLE : std_logic_vector(7 downto 0) := "01000111";
+  constant Y_PREAMBLE : std_logic_vector(7 downto 0) := "00100111";
+  constant Z_PREAMBLE : std_logic_vector(7 downto 0) := "00010111";
 
-    type byte_2 is record
-        aux_sample_bits     : std_logic_vector(2 downto 0);
-        sample_word_length  : std_logic_vector(2 downto 0);
-        alignment_level     : std_logic_vector(1 downto 0);
-    end record byte_2;
+  -- AES3/EBU or SPDIF stream constants
+  constant FRAMES : integer := 192;
+  subtype FRAME_WIDTH is integer range FRAMES-1 downto 0;
+  constant CHANNELS : integer := 2;
+  constant SUBFRAME : integer := 32;
+
+  -- channel status description
+  type byte_0 is record
+    channel_use : std_logic;
+    pcm         : std_logic;
+    preemphasis : std_logic_vector(2 downto 0);
+    --
+    sample_frequency : std_logic_vector(1 downto 0);
+  end record byte_0;
+
+  type byte_1 is record
+      channel_mode : std_logic_vector(3 downto 0);
+      user_bit     : std_logic_vector(3 downto 0);
+  end record byte_1;
+
+  type byte_2 is record
+      aux_sample_bits    : std_logic_vector(2 downto 0);
+      sample_word_length : std_logic_vector(2 downto 0);
+      alignment_level    : std_logic_vector(1 downto 0);
+  end record byte_2;
 
     type byte_3 is record
         multichannel_mode   : std_logic_vector(6 downto 0);
@@ -87,7 +105,7 @@ package spdif_pkg is
         crc : std_logic_vector(7 downto 0);
     end record channel_status;
 
-    -- RX data will be 5 times oversampled
+    -- RX data will be 8 times oversampled
     -- 192000 Hz : 24,576  MHz
     -- 176400 Hz : 22,5792 MHz
     --  96000 Hz : 12,288  MHz
@@ -95,5 +113,18 @@ package spdif_pkg is
     --  48000 Hz :  6,144  MHz
     --  44100 Hz :  5,6448 MHz
     --  32000 Hz :  4,096  MHz
+
+-------------------------------------------------------------------------------
+-- Components
+-------------------------------------------------------------------------------
+
+  component aes_ebu_rx
+  port (
+    clk     : in std_ulogic;
+    reset_n : in std_ulogic;
+    aes_in  : in t_aes_in;
+    aes_out : out t_aes_out
+  );
+  end component;
 
 end spdif_pkg;
