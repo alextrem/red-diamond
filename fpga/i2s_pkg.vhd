03e5926bb77a2e8dafbdf771abee953498416808
Alexander Geißler <lancemt@gmx.de>
2016-11-27 21:34:40 +0100

    initial commit

---
+++ fpga/i2s_pkg.vhd
@@ -0,0 +1,30 @@
+------------------------------------------------------------------------------
+-- Company:          Red Diamond
+-- Engineer:         Alexander Geißler
+--
+-- Create Date:      23:40:00 11/27/2016
+-- Design Name:      i2s_pkg.vhd
+-- Project Name:     red-diamond
+-- Target Device:    EP4CE22C8N
+-- Tool Versions:    16.0
+-- Description:      This I2S package contains a
+--
+-- Dependencies:
+--
+-- Revision:
+-- Revision 0.1 - File created
+------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+package i2s_pkg is
+
+  type t_i2s_in is record
+  end record;
+
+  type t_i2s_out is record
+  end record;
+
+
+end i2s_pkg;
