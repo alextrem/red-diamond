fb6ad0cce47ea2da401e6482601a5f553cd78a13
Alexander Geißler <lancemt@gmx.de>
2016-12-15 22:16:01 +0100

    minor changes an removing unnecessary stuff

--- fpga/hdmi.vhd
+++ fpga/hdmi.vhd
@@ -1,32 +1,32 @@
----------------------------------------
--- Company:
--- Engineer:        Alexander Geißler
+------------------------------------------------------------------------------
+-- Company:          Red Diamond
+-- Engineer:         Alexander Geißler
 --
--- Create Date:     23:40:00 02/26/2015
--- Design Name:
--- Project Name:    red-diamond
--- Target Device:   EP4CE22C8N
--- Tool Versions:   14.0
--- Description:     Audio interface of
--- ADV7612
+-- Create Date:      23:40:00 11/26/2016
+-- Design Name:      hdmi.vhd
+-- Project Name:     red-diamond
+-- Target Device:    EP4CE22C8N
+-- Tool Versions:    16.0
+-- Description:      This module interfaces an hdmi receiver
 --
 -- Dependencies:
 --
 -- Revision:
 -- Revision 0.1 - File created
---------------------------------------
+------------------------------------------------------------------------------
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+
+library ieee;
+use ieee.std_logic_1164.ALL;
 
 entity hdmi is
     port (
         -- Synchrounous reset
-        reset   : out std_logic;
+        reset   : out std_ulogic;
         -- I2C interface
-        scl     : out std_logic;
-        sda     : inout std_logic;
-        csn     : out std_logic;
+        scl     : out std_ulogic;
+        sda     : inout std_ulogic;
+        csn     : out std_ulogic;
         -- I2S interface
         -- Data outputclock for left and right channel
         lrclk   : in std_logic;
@@ -41,13 +41,10 @@ entity hdmi is
         ap4     : in std_logic;
         -- interrupt
         interrupt     : in std_logic_vector(1 downto 0)
-         );
+        );
 end entity;
 
 architecture rtl of hdmi is
-
-    type state is (idle);
-    signal current_state : state:=idle;
 
 begin
 
