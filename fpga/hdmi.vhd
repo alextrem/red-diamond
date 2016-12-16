8caa14085f49d3441d13b537f069c6fc17ca3d0a
Alexander Geißler <lancemt@gmx.de>
2016-12-16 21:25:03 +0100

    set input and output records

--- fpga/hdmi.vhd
+++ fpga/hdmi.vhd
@@ -18,30 +18,17 @@
 
 library ieee;
 use ieee.std_logic_1164.ALL;
+use work.hdmi_pkg.all;
 
 entity hdmi is
-    port (
-        -- Synchrounous reset
-        reset   : out std_ulogic;
-        -- I2C interface
-        scl     : out std_ulogic;
-        sda     : inout std_ulogic;
-        csn     : out std_ulogic;
-        -- I2S interface
-        -- Data outputclock for left and right channel
-        lrclk   : in std_logic;
-        -- Bit clock
-        sclk    : in std_logic;
-        -- Audio master clock output
-        mclk    : in std_logic;
-        ap0     : in std_logic;
-        ap1     : in std_logic;
-        ap2     : in std_logic;
-        ap3     : in std_logic;
-        ap4     : in std_logic;
-        -- interrupt
-        interrupt     : in std_logic_vector(1 downto 0)
-        );
+  port (
+    -- Synchrounous reset
+    reset   : in std_logic;
+    mclk    : in std_logic;
+
+    hdmi_in : in t_hdmi_in;
+    hdmi_out : out t_hdmi_out
+  );
 end entity;
 
 architecture rtl of hdmi is
--- fpga/hdmi_pkg.vhd
+++ fpga/hdmi_pkg.vhd
@@ -16,14 +16,34 @@
 ------------------------------------------------------------------------------
 
 library ieee;
-use ieee.std_logic_1164.all
+use ieee.std_logic_1164.all;
 
-package hdmi is
+package hdmi_pkg is
   type t_hdmi_out is record
     -- I2C Interface
     scl : std_ulogic;
     sda : std_ulogic;
     csn : std_logic;
 
+    reset : std_logic;
   end record t_hdmi_out;
+
+  type t_hdmi_in is record
+    lrck : std_logic; -- word clock
+    sclk : std_logic; -- Bit clock
+    ap   : std_logic_vector(4 downto 0);
+    int  : std_logic; --interrupt
+  end record t_hdmi_in;
+
+
+  component hdmi is
+    port (
+      reset    : in std_logic;
+      mclk     : in std_logic;
+
+      hdmi_in  : in t_hdmi_in;
+      hdmi_out : out t_hdmi_out
+    );
+  end component;
+
 end package;
