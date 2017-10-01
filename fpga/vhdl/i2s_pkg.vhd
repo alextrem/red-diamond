cfaf9a10fafa68be84cb172f8850b82ea94b2e64
Alexander Geißler <lancemt@gmx.de>
2017-10-01 23:23:29 +0200

    changed indentation added first documentation steps

--- fpga/vhdl/i2s_pkg.vhd
+++ fpga/vhdl/i2s_pkg.vhd
@@ -16,31 +16,41 @@
 -- Revision 0.2 - Added sinus lookup table creation
 ------------------------------------------------------------------------------
 
+--! standard library
 library ieee;
 use ieee.math_real.all;
+--! standard signal types
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 
+--! @brief   I2S types definition
+--! @details This package contains constants, type definitions and subprograms
+--!          for the I2S transmitter and receiver interface.
+--!          This package contains a built in NCO to create testsignals.
 package i2s_pkg is
 
 -------------------------------------------------------------------------------
 -- Constants
 -------------------------------------------------------------------------------
 
-  constant c_addr    : integer := 12;
+   constant c_addr    : integer := 12;
 
-  type t_i2s_in is record
-    l_channel : std_logic_vector(23 downto 0);
-    r_channel : std_logic_vector(23 downto 0);
-  end record;
+   type t_i2s_in is record
+      slv_l_channel : std_logic_vector(23 downto 0);
+      slv_r_channel : std_logic_vector(23 downto 0);
+   end record;
 
-  type t_i2s_out is record
-    wclk  : std_ulogic;
-    bclk  : std_ulogic;
-    sdata : std_logic;
-  end record;
+   type t_i2s_out is record
+      sl_wclk  : std_ulogic;
+      sl_bclk  : std_ulogic;
+      sl_sdata : std_ulogic;
+   end record;
 
-  type mem_array is array(0 to (2**c_addr)-1) of std_logic_vector(23 downto 0);
+   type mem_array is array(0 to (2**c_addr)-1) of std_logic_vector(23 downto 0);
+
+   subtype t_i2s_vendor is integer range 0 to 16#ff#;
+   subtype t_i2s_device is integer range 0 to 16#ff#;
+   subtype t_i2s_version is integer range 0 to 16#01#;
 
 -------------------------------------------------------------------------------
 -- Functions
@@ -53,33 +63,34 @@ package i2s_pkg is
 -- Components
 -------------------------------------------------------------------------------
 
-  component i2s_tx
-  port (
-    reset_n : in std_logic;
-    mclk    : in std_logic;
-    --
-    i2s_in  : in t_i2s_in;
-    i2s_out : out t_i2s_out
-  );
-  end component;
+   component i2s_tx
+      generic ( DATA_WIDTH : integer range 16 to 24 := 24);
+   port (
+      reset_n : in std_logic;
+      mclk    : in std_logic;
+      --
+      r_i2s_in  : in t_i2s_in;
+      r_i2s_out : out t_i2s_out
+   );
+   end component;
 
 end i2s_pkg;
 
 package body i2s_pkg is
 
-  function cos_lut
-  return mem_array is
-    constant N     : integer := 2**c_addr;
-    constant N1    : real := real(N);
-    variable w, k1 : real;
-    variable memx  : mem_array;
-  begin
-    for k in 0 to N-1 loop
-      k1      := (real(k)+0.5)/N1;
-      w       := cos(math_pi_over_2 * k1); -- first quadrant of cosine wave
-      memx(k) := std_logic_vector(conv_signed(integer(round(8388608.0*w)),24));
-    end loop;
-    return memx;
+   function cos_lut
+   return mem_array is
+      constant N     : integer := 2**c_addr;
+      constant N1    : real := real(N);
+      variable w, k1 : real;
+      variable memx  : mem_array;
+   begin
+      for k in 0 to N-1 loop
+         k1      := (real(k)+0.5)/N1;
+         w       := cos(math_pi_over_2 * k1); -- first quadrant of cosine wave
+         memx(k) := std_logic_vector(conv_signed(integer(round(8388608.0*w)),24));
+      end loop;
+      return memx;
   end function cos_lut;
 
 end;
