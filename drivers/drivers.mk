7874d2ea6ac3404328847ba4d773b4e10ed1f71f
Alexander Geißler <lancemt@gmx.de>
2018-01-12 23:39:28 +0100

    added sources and include files to makefile

--- drivers/drivers.mk
+++ drivers/drivers.mk
@@ -4,12 +4,14 @@ DRIVERPATH = drivers
 # All subdirectories end up here
 DRIVERDIRS = dac \
              hdmi \
+			 sensors \
 
 # Source files
 
 DRIVERSRC = $(DRIVERPATH)/dac/cs43l22.c \
             $(DRIVERPATH)/dac/pcm1792a.c \
-            $(DRIVERPATH)/hdmi/adv7612.c
+            $(DRIVERPATH)/hdmi/adv7612.c \
+            $(DRIVERPATH)/sensors/max30101.c
 
 # Include files for drivers
 DRIVERINC = $(foreach inc,$(DRIVERDIRS),$(DRIVERPATH)/$(inc)/inc)
