d1c16081e3a427a1a4f3af501fdb5b6412a85964
Alexander Geißler <lancemt@gmx.de>
2018-01-14 21:00:23 +0100

    removed PHONY and default declaration

--- extract.mk
+++ extract.mk
@@ -5,9 +5,5 @@ print-% : ;@echo $* = $($*)
 EXTERNAL = ChibiOS/ext
 TARFILES = $(wildcard $(EXTERNAL)/*.7z)
 
-default: extract
-
 extract: $(TARFILES)
 	$(warning $(foreach file, $(TARFILES), $(shell 7z x -y -o$(EXTERNAL) $(file))))
-
-.PHONY: default
